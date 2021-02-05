8a179c8e717142654e0afa01ccc4299ddac61e7e
Moinul_Hasan <moinul107@gmail.com>
2021-02-05 12:17:07 +0800

    changes in pointers

--- pointers.go
+++ pointers.go
@@ -6,8 +6,8 @@ func main () {
 
 	var x int = 18
 	var y int
-	var ip *int 
-	ip = &x
-	y = *ip
-fmt.Println(y)
+	var ip *int  //ip is pointer to int
+	ip = &x //ip now points to x
+	y = *ip // y is now 18
+	fmt.Println(y)
 }
