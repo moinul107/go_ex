9eaf7f94ea0bb8f3efe3122a828b4499c78a582e
Moinul_Hasan <moinul107@gmail.com>
2021-02-05 09:45:33 +0800

    Hello GO

---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main(){
+	fmt.Printf("H B!")
+	fmt.printf("Alr then")
+}
---
+++ syn_var_error.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main(){
+
+var name string = "Moinul"
+var name2, city string = "Moinul" , "CTG"
+fmt.Println(name, city, name2)
+
+
+//char
+var c rune = 'A'
+var age int = 10
+var result float32 = 50.95
+fmt.Println(c, age, result)
+}
