ac7c887a020a75844b2a82e9d20d1e5912fcc0dd
Moinul_Hasan <moinul107@gmail.com>
2021-02-06 12:50:39 +0800

    primitive data types

---
+++ fmt_pkg_verbs.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// var name string
+	// var age int
+	// var result float32
+	// var isFound bool
+	// fmt.Println(name, age, result, chr, isFound)
+
+	var chr rune
+	chr = 'F'
+	fmt.Println(chr)
+	fmt.Printf("%c", chr)
+
+}
