9260e7ffbba6dfaa4ae5ef425afabd411dc2652e
Moinul_Hasan <moinul107@gmail.com>
2021-02-06 13:35:53 +0800

    primitive data types

--- fmt_pkg_verbs.go
+++ fmt_pkg_verbs.go
@@ -7,8 +7,14 @@ func main() {
 	// var name string
 	// var age int
 	// var result float32
-	// var isFound bool
+	var isFound bool
 	// fmt.Println(name, age, result, chr, isFound)
+	fmt.Println(isFound)
+	fmt.Println(true && false)
+	fmt.Println(true && true)
+	fmt.Println(true || true)
+	fmt.Println(true || false)
+	fmt.Println(!true)
 
 	var chr rune
 	chr = 'F'
